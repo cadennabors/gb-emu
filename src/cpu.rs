dc2eba020a2bb9ac0422cb3b815e77b852d8ea72
cadennabors <78510667+cadennabors@users.noreply.github.com>
2025-10-19 22:40:14 -0500

    Start building architecture for CPU

---
+++ src/cpu.rs
@@ -0,0 +1,20 @@
+struct Registers {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    h: u8,
+    l: u8,
+}
+
+impl Registers {
+    fn get_af(&self) -> u16 {
+        ((self.a as u16) << 8) | (self.f as u16)
+    }
+    fn set_af(&mut self, val : u16) {
+        self.a = ((val & 0xFF00) >> 8) as u8;
+        self.f = (val & 0x00FF) as u8;
+    }
+}
