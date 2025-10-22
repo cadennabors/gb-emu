6a70e7ad5eabdb47d9a34b6b1e1ce0567c6899f5
cadennabors <78510667+cadennabors@users.noreply.github.com>
2025-10-21 23:12:19 -0500

    Update cpu.rs

--- src/cpu.rs
+++ src/cpu.rs
@@ -20,20 +20,44 @@ enum Reg16b {
 }
 
 impl Registers {
-    fn combine_bytes(hi: &u8, lo: &u8) -> u16 {
-        ((*hi as u16) << 8) | (*lo as u16)
+    fn combine_bytes(hi: u8, lo: u8) -> u16 {
+        ((hi as u16) << 8) | (lo as u16)
     }
-    fn split_bytes(hi: &mut u8, lo: &mut u8, val: u16) {
-        *hi = ((val & 0xFF00) >> 8) as u8;
-        *lo = (val & 0x00FF) as u8;
+    fn split_bytes(val: u16) -> (u8, u8) {
+        let hi = ((val & 0xFF00) >> 8) as u8;
+        let lo = (val & 0x00FF) as u8;
+        (hi, lo)
     }
 
     fn get_16(&self, register: Reg16b) -> u16 {
+        let (hi, lo) = match register {
+            Reg16b::AF => (&self.a, &self.f),
+            Reg16b::BC => (&self.b, &self.c),
+            Reg16b::DE => (&self.d, &self.e),
+            Reg16b::HL => (&self.h, &self.l),
+        };
+        Self::combine_bytes(*hi, *lo)
+    }
+
+    fn set_16(&mut self, register: Reg16b, val: u16) {
+        let (hi, lo) = Self::split_bytes(val);
         match register {
-            Reg16b::AF => Self::combine_bytes(&self.a, &self.f),
-            Reg16b::BC => Self::combine_bytes(&self.b, &self.c),
-            Reg16b::DE => Self::combine_bytes(&self.d, &self.e),
-            Reg16b::HL => Self::combine_bytes(&self.h, &self.l),
+            Reg16b::AF => {
+                self.a = hi;
+                self.f = lo;
+            }
+            Reg16b::BC => {
+                self.b = hi;
+                self.c = lo;
+            }
+            Reg16b::DE => {
+                self.d = hi;
+                self.e = lo;
+            }
+            Reg16b::HL => {
+                self.h = hi;
+                self.l = lo;
+            }
         }
     }
 }
