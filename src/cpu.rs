6d945b9b289b4d466c647e154cf219b2e90b29ef
cadennabors <78510667+cadennabors@users.noreply.github.com>
2025-10-21 22:49:29 -0500

    add enums for 16 bit registers

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,6 @@
 struct Registers {
+    pc: u16,
+    sp: u16,
     a: u8,
     b: u8,
     c: u8,
@@ -9,12 +11,30 @@ struct Registers {
     l: u8,
 }
 
+enum Reg16b {
+    AF,
+    BC,
+    DE,
+    HL,
+}
+
 impl Registers {
-    fn get_16_bit_register(first_register : &u8, second_register : &u8) -> u16 {
-        ((*first_register as u16) << 8) | (*second_register as u16)
+    fn combine_bytes(hi : &u8, lo : &u8) -> u16 {
+        ((*hi as u16) << 8) | (*lo as u16)
     }
-    fn set_16_bit_register(first_register : &mut u8, second_register : &mut u8, val : u16) {
-        *first_register = ((val & 0xFF00) >> 8) as u8;
-        *second_register = (val & 0x00FF) as u8;
+    fn split_bytes(hi : &mut u8, lo : &mut u8, val : u16) {
+        *hi = ((val & 0xFF00) >> 8) as u8;
+        *lo = (val & 0x00FF) as u8;
     }
+
+
+    fn get_16(&self, register: Reg16b) -> u16 {
+        match register {
+            Reg16b::AF => Self::combine_bytes(&self.a, &self.f),
+            Reg16b::BC => Self::combine_bytes(&self.b, &self.c),
+            Reg16b::DE => Self::combine_bytes(&self.d, &self.e),
+            Reg16b::HL => Self::combine_bytes(&self.h, &self.l),
+        }
+    }
+
 }
